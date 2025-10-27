c2a8c16b92475f47574506b1f02e9d7ba42f99e5
Lakshya Sunkara <lakshyasunkara2004@gmail.com>
2025-10-28 00:41:54 +0530

    update in mail sender

--- server.js
+++ server.js
@@ -106,12 +106,15 @@ const OWNER_EMAIL = 'sunkarassnaidu@gmail.com';
 const OWNER_PASSWORD = 'Sunkara@2004';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // use TLS
   auth: {
-    user: 'sunkarassnaidu@gmail.com',
-    pass: process.env.GMAIL_APP_PASSWORD 
+    user: process.env.BREVO_USER, // your Brevo email
+    pass: process.env.BREVO_PASS  // your Brevo API key
   }
 });
+
 
 async function notifyOwner(subject, message) {
   try {
